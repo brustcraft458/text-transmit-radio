84b91106d240e2702ce5ffd9076962e39fc34795
Bisma Candra <84393872+brustcraft458@users.noreply.github.com>
2024-01-01 00:00:02 +0700

    Update Picth

--- picth.js
+++ picth.js
@@ -1,12 +1,16 @@
 // Audio context
 const AudioContext = window.AudioContext || window.webkitAudioContext
-var audioCtx, gainNode, osc
+var audioCtx, gainNode, osciNode
 
 // Init Component
+const icon = document.querySelector("#icon-radio")
 const form = document.querySelector("#text-form")
 const formInput = form.querySelector("#text")
-const formSubmit = form.querySelector("#button-submit")
-formSubmit.addEventListener("click", () => {
+const formButton = form.querySelector("#button-submit")
+const connectButton = form.querySelector("#button-connect")
+
+// Button
+formButton.addEventListener("click", () => {
     const binary = textToBinary(formInput.value)
     console.log(formInput.value)
     console.log("binary data: ", binary)
@@ -15,38 +19,54 @@ formSubmit.addEventListener("click", () => {
     playback(binary)
 })
 
+connectButton.addEventListener("click", async() => {
+    await playback("000000000000000000000000")
+    await playback("111111111111111111111111")
+    await playback("                        ")
+})
+
 // Play Audio
 async function playback(binaryData) {
-    const timeDelay = 800
+    const timeDelay = 350
+    icon.classList.remove("border-secondary")
+
     for (let i = 0; i < binaryData.length; i++) {
         const bin = binaryData[i]
         
         // Play
         if (bin == '1') {
-            await playNote(1000, 1.0, timeDelay)
+            icon.classList.add("border-primary")
+            await playNote(500, 1.0, timeDelay)
+            icon.classList.remove("border-primary")
         } else if (bin == '0') {
-            await playNote(1000, 0.1, timeDelay)
+            icon.classList.add("border-warning")
+            await playNote(500, 0.1, timeDelay)
+            icon.classList.remove("border-warning")
+        } else {
+            await playNote(500, 0.035, timeDelay * 2)
         }
 
         // End
         await playNote(0, 0.0, timeDelay)
     }
+
+    icon.classList.add("border-secondary")
 }
 
 // Note
 async function playNote(note, volume, time) {
-    if (!osc || !osc.context || osc.context.state != 'running') {
+    if (!osciNode || !osciNode.context || osciNode.context.state != 'running') {
         // Create oscillator
         audioCtx = new AudioContext()
-        osc = audioCtx.createOscillator()
+        osciNode = audioCtx.createOscillator()
         gainNode = audioCtx.createGain();
 
-        osc.connect(gainNode);
+        osciNode.connect(gainNode);
         gainNode.connect(audioCtx.destination);
-        osc.start()
+        osciNode.start()
     }
 
-    osc.frequency.setValueAtTime(note, audioCtx.currentTime)
+    osciNode.frequency.setValueAtTime(note, audioCtx.currentTime)
     gainNode.gain.setValueAtTime(volume, audioCtx.currentTime);
 
     await sleep(time)
